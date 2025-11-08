5719562ecf462c0d0801d0802b10c168ea49735e
bmeg2001 present <samlee3823423@gmail.com>
2025-11-08 22:41:19 +0800

    Update vite config to build to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/bmeg2001_group1_present/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
